4865ce19a339a939d490c9fc85e45eface98e4f6
yashu <yaseswiniy@gmail.com>
2022-12-07 16:46:59 +0530

    Before-After --Suite , Test and Class

--- src/test/java/sample/Login.java
+++ src/test/java/sample/Login.java
@@ -1,20 +1,48 @@
 package sample;
 
 import org.openqa.selenium.By;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Login {
 	WebDriver driver;
-	
+	@BeforeSuite
+	public void beforeSuite() {
+		System.out.println("Before Suite");
+	}
+	@AfterSuite
+	public void afterSuite() {
+		System.out.println("After Suite");
+	}
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("Before Test");
+	}
+	@AfterTest
+	public void afterTest() {
+		System.out.println("After Test");
+	}
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println("Before Class");
+	}
+	@AfterClass
+	public void afterClass() {
+		System.out.println("After Class");
+	}
 	@BeforeMethod
 	public void setUp() {
 		WebDriverManager.chromedriver().setup();

5f41d2c12b2546daf5bc83a1fe14ee1e32d6dae6
yashu <yaseswiniy@gmail.com>
2022-12-08 12:38:39 +0530

    Cross - Browser in TestNG

---
+++ src/test/java/cross_Browser/MultiBrowser.java
@@ -0,0 +1,39 @@
+package cross_Browser;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+public class MultiBrowser {
+	
+	public WebDriver driver;
+	@Parameters("browser")
+	@BeforeClass
+	public void beforeTest(String browser) {
+		
+		if(browser.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		}else if(browser.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		}
+		driver.get("https://demoqa.com/");
+	}
+	@Test
+	public void click_Carousel() throws InterruptedException {
+		
+		WebElement carousel = driver.findElement(By.xpath("//img[@class='banner-image']"));
+		carousel.click();
+		Thread.sleep(3000);
+	}
+	@AfterClass
+	public void afterTest() {
+		driver.quit();
+	}
+}

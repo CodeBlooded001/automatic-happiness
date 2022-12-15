40825c9bf7848d89842777083f72de909e6152f2
yashu <yaseswiniy@gmail.com>
2022-12-15 13:45:22 +0530

    Grouping - Meta Groups and Notes

--- src/test/java/grouping/LoginGroup.java
+++ src/test/java/grouping/LoginGroup.java
@@ -6,23 +6,23 @@ public class LoginGroup {
 
 	@Test(priority=1,groups= {"smoke","sanity","regression","login","all"})
 	public void LoginWithValidCredentials() {
-		System.err.println("Login With Valid Credentials - Login");
+		System.out.println("Login With Valid Credentials - Login");
 	}
 	@Test(priority=2,groups= {"sanity","regression","login","all"})
 	public void LoginWithInValidCredentials() {
-		System.err.println("Login With Invalid Credentials - Login");
+		System.out.println("Login With Invalid Credentials - Login");
 	}
 	@Test(priority=3,groups= {"sanity","regression","login","all"})
 	public void LoginWithValidUserNameAndInvalidPassword() {
-		System.err.println("Login With Valid UserName And Invalid Password- Login");
+		System.out.println("Login With Valid UserName And Invalid Password- Login");
 	}
 	@Test(priority=4,groups= {"sanity","regression","login","all"})
 	public void LoginWithInValidUserNameAndValidPassword() {
-		System.err.println("Login With InValid UserName And Valid Password - Login");
+		System.out.println("Login With InValid UserName And Valid Password - Login");
 	}
 	@Test(priority=5,groups= {"smoke","regression","login","all"})
 	public void LoginWithNoCredentials() {
-		System.err.println("Login With No Credentials - Login");
+		System.out.println("Login With No Credentials - Login");
 	}
 	
 }
